cadf954afae72a62c9c6a66fd207024a9f429c6d
Shoaib Areeb <sareeb50@gmail.com>
2019-08-31 11:55:03 +0530

    created test case to check whether function is working

--- SRIP/Codes/segment.js
+++ SRIP/Codes/segment.js
@@ -1,17 +1,66 @@
-var slider = document.getElementById("myRange1");
-var output = document.getElementById("MeanDeviation");
-output.innerHTML = slider.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-} 
+var T2;
+var T1;
+var Histo;
+var RunButton;
+var TypeOfSeg;
+var canvas;
+var context;
 
-var slider2 = document.getElementById("myRange2");
-var output2 = document.getElementById("VarianceDeviation");
-output2.innerHTML = slider2.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
-slider2.oninput = function() {
-  output2.innerHTML = this.value;
-} 
+function preview( img , img2)
+	{
+        document.getElementById("input_layer").style.visibility="visible";
+        
+        document.getElementById("input_layer").setAttribute("src",img);
+        document.getElementById("input_layer").setAttribute("height",300);
+        document.getElementById("input_layer").setAttribute("width",300);
+        showhist(img2);
+        init();
+	}
+
+    function showhist(img)
+    {
+          document.getElementById("Hist").setAttribute("src",img);
+          document.getElementById("Hist").setAttribute("height",200);
+          document.getElementById("Hist").setAttribute("width",350);
+    }
+
+    function drawImage(image) 
+     {
+        context.drawImage(image, 0, 0);
+     }
+    function init() {
+            var image = document.getElementById("input_layer");
+            //var secimage = document.getElementById("secondary")
+            RunButton = document.getElementById("Run");
+            canvas = document.getElementById("output_layer");
+            context = canvas.getContext("2d");  
+            // Set the canvas the same width and height of the image
+            canvas.width = image.width;
+            canvas.height = image.height;
+            RunButton.addEventListener("click", addEffect);
+        }
+
+    function addEffect() {
+    
+    
+    drawImage(document.getElementById("input_layer"));
+    var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    
+    doOperation(imageData.data);
+    context.putImageData(imageData, 0, 0);
+}
+
+function doOperation(data)
+{
+	TypeOfSeg = $("input:radio[name=TypeSeg]:checked").val();
+     Histo = $("input:radio[name=Hist]:checked").val();
+
+     for (var i = 0; i < data.length; i+= 4) {
+     data[i] = 255; // Invert Red
+     data[i+1] =  255; // Invert Green
+     data[i+2] =  255; // Invert Blue
+      }
+}
+
